db92782bc81d336cdfba47fd397176f6ce772986
leolivm <leolivm@outlook.com>
2021-07-23 09:09:17 -0300

    feat(NossasLojas): 🛍 init component (23700910)

--- react/components/NossasLojas/index.tsx
+++ react/components/NossasLojas/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState, useRef, FormEvent } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactLoading from "react-loading";
 
 // Styles
