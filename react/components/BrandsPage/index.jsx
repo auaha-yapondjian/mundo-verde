0dc6d215b77b462b3f399a04c6e97c200ad3c669
Diogo Rosa <diogo.duarte@corebiz.ag>
2022-03-09 16:17:55 -0300

    feat(brands): creating page

---
+++ react/components/BrandsPage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import GET_BRANDS from '../../graphql/getBrands.graphql'
+
+// import { useQuery } from 'react-apollo'
+
+function BrandsPage() {
+  // const { data } = useQuery(GET_BRANDS, { ssr: false })
+
+  // console.log('DATA: ', data)
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default BrandsPage
