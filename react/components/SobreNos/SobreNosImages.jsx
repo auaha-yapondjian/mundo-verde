170357dc1a78ce432b7193e97db8d8d2050d5653
Diogo Rosa <diogo.duarte@corebiz.ag>
2022-04-19 09:42:32 -0300

    adding iframes

--- react/components/SobreNos/SobreNosImages.jsx
+++ react/components/SobreNos/SobreNosImages.jsx
@@ -21,18 +21,102 @@ function SobreNosImages({
         <div className="fourth-row-sobrenos-content">
           <h1>{titleOfImages}</h1>
           <div className="images-sobrenos-content">
-            <img src={image1} alt="imagem 1" />
-            <img src={image2} alt="imagem 2" />
-            <img src={image3} alt="imagem 3" />
-            <img src={image4} alt="imagem 4" />
-            <img src={image5} alt="imagem 5" />
-            <img src={image6} alt="imagem 6" />
-            <img src={image7} alt="imagem 7" />
-            <img src={image8} alt="imagem 8" />
-            <img src={image9} alt="imagem 9" />
-            <img src={image10} alt="imagem 10" />
-            <img src={image11} alt="imagem 11" />
-            <img src={image12} alt="imagem 12" />
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cbh4XPmvgpP/embed"
+              width="166"
+              height="240"
+              frameborder="0"
+              scrolling="no"
+              allowtransparency="true"
+            ></iframe>
           </div>
         </div>
       </div>
